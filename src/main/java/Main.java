ce923eb0405377127303f4406b1ca1ec0aab5d7a
Abramov-GR <gabramov89@gmail.com>
2022-05-22 09:34:40 +0300

    Changing input from local file to reading from url

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,30 +2,29 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import java.io.InputStreamReader;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.*;
+import java.util.concurrent.ExecutorCompletionService;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class Main {
 
     private static final Logger LOG = LogManager.getLogger(Main.class);
 
     public static void main(String[] args) {
-//        ExecutorService executorService = Executors.newFixedThreadPool(8);
         ExecutorService executorService = Executors.newCachedThreadPool();
         try {
             ExecutorCompletionService<Map<CheckedName, List<PositionInText>>> executorCompletionService = new ExecutorCompletionService<>(executorService);
             long startTime = System.currentTimeMillis();
 
-            File textFile = new File("test.txt");
-//            URL url = new URL("http://norvig.com/big.txt");
-//            FileUtils.copyURLToFile(url, textFile);
+            URL url = new URL("http://norvig.com/big.txt");
 
             int numberOfTasks = 0;
-            try (BufferedReader br = new BufferedReader(new FileReader(textFile))) {
+            try (BufferedReader br = new BufferedReader(new InputStreamReader(url.openStream()))) {
                 String st;
                 List<Line> linesForMatcher = new ArrayList<>();
 
